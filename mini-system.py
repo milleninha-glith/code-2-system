fac0ef868bb856c6dc0504027c514cd7b7c337e5
mi <grisa.millena@gmail.com>
2025-10-31 11:10:37 -0300

    Update mini-system.py

--- mini-system.py
+++ mini-system.py
@@ -3,25 +3,30 @@ print("** MINI SISTEMA DE USUÁRIO **\n")
 def cadastro_new_user():
     print("CADASTRO DE NOVOS USUÁRIOS: \n")
     new_username = input("Digite seu username: ")
-    new_password = input("Digite sua nova senha: ")
+    new_password = input("Olá", new_password, "! Digite sua nova senha: ")
     nova_senha = new_password
-    new_password = input("Confirme sua senha: ")
+    new_password = input(new_password, ", Confirme sua senha: ")
     while new_password != nova_senha:
         new_password = input("Tente novamente! Digite sua nova senha: ")
-    print("Olá, ", new_username, ". Você foi cadastrado!")
+    print (new_username, ", Você foi cadastrado!")
 
+def user_cadastrado():
+    nome_cad = "vini"
+    senha_cad = "123"
+
+    username_cad = input("Digite seu usuário cadastrado: ")
+    password_cad = input("Digite sua senha cadastrada: ")
+
+    if username_cad == nome_cad:
+        while password_cad != senha_cad:
+                password_cad = input("Tente novamente! Digite sua senha cadastrada: ")
+        print("Usuário Logado no MINI SYSTEM")
+    else:
+        print("Usuário não cadastrado!")
 opcao_inicial = int(input("Digite 1 para fazer LOGIN ou Digite 2 para CADASTRO: \n"))
 if opcao_inicial == 1 or opcao_inicial == 2:
     if opcao_inicial == 1:
-        nome_cad = "mi"
-        senha_cad = "1996"
-
-        username_cad = input("Digite seu usuário cadastrado: ")
-        password_cad = input("Digite sua senha cadastrada: ")
-
-        if username_cad == nome_cad:
-            while password_cad != senha_cad
-        print("Usuário Logado no MINI SYSTEM")
+        user_cadastrado()
     else:
         cadastro_new_user()
 else:
