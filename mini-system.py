c5c92b755809c907ee901dbf48679740683759b1
mi <grisa.millena@gmail.com>
2025-11-04 07:57:01 -0300

    Add files via upload

--- mini-system.py
+++ mini-system.py
@@ -4,8 +4,9 @@ def cadastro_new_user():
     print("CADASTRO DE NOVOS USUÁRIOS: \n")
     new_username = input("Digite seu username: ")
     new_password = input("Olá", new_password, "! Digite sua nova senha: ")
-    nova_senha = new_password
-    new_password = input(new_password, ", Confirme sua senha: ")
+
+    new_password = input(new_username, ", Confirme sua senha: ")
+
     while new_password != nova_senha:
         new_password = input("Tente novamente! Digite sua nova senha: ")
     print (new_username, ", Você foi cadastrado!")
@@ -23,7 +24,8 @@ def user_cadastrado():
         print("Usuário Logado no MINI SYSTEM")
     else:
         print("Usuário não cadastrado!")
-opcao_inicial = int(input("Digite 1 para fazer LOGIN ou Digite 2 para CADASTRO: \n"))
+        def definir_opcao(){ 
+      opcao_inicial = int(input("Digite 1 para fazer LOGIN ou Digite 2 para CADASTRO: \n"))
 if opcao_inicial == 1 or opcao_inicial == 2:
     if opcao_inicial == 1:
         user_cadastrado()
@@ -31,6 +33,5 @@ if opcao_inicial == 1 or opcao_inicial == 2:
         cadastro_new_user()
 else:
     print("Opção não encontrada! Tente novamente mais tarde.")
-
-
+ }
 
